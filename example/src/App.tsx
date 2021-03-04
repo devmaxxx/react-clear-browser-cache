2e56b2e3cef271151f331ba8eb068bd7fb838c0d
msynEfisco <msyn@efisco.net>
2021-03-04 19:29:42 +0200

    chore: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+
 // import ChunkLoadError from './ChunkLoadError';
+
+//@ts-ignore
+let LazyComponent = lazy(() => import('./Component'));
 
 const App = () => {
   React.useEffect(() => {
@@ -10,7 +14,16 @@ const App = () => {
     // throw new SyntaxError("' < '");
   }, []);
 
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <div>
+        <Suspense fallback='Loading...'>
+          <LazyComponent>Lazy component</LazyComponent>
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ example/src/Component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Component({ children }: any) {
+  return <div>{children}</div>;
+}
