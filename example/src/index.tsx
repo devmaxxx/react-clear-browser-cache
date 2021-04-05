f8c7730dc44b11a609edf2c6c11e72f6bfbb07af
arenuzzz <syn.max.13@gmail.com>
2021-04-05 17:37:36 +0300

    Fix: fix localstorage error

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,7 +5,14 @@ import {
   ClearBrowserCacheBoundary,
   ClearBrowserCacheDebugFunc
 } from 'react-clear-browser-cache';
+import engine from 'store/src/store-engine';
+//@ts-ignore
+import allStorages from 'store/storages/all';
+
 import App from './App';
+
+// create store
+const store = engine.createStore(allStorages, []);
 
 const debug: ClearBrowserCacheDebugFunc = (data) => {
   console.log(data);
@@ -17,6 +24,8 @@ ReactDOM.render(
       fallback='Loading...'
       auto
       duration={5000}
+      //@ts-ignore
+      storage={store}
       // duration={5 * 60 * 1000}
       debug={debug}
     >
